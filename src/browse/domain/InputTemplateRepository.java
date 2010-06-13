5840f7f23e3c368f9d5d7ed86b1491efcee1caa0
Jonas Wiklund <jwiklund@gmail.com>
2010-06-13 19:13:19 +0200

    Removed unused import

--- src/browse/domain/InputTemplateRepository.java
+++ src/browse/domain/InputTemplateRepository.java
@@ -10,7 +10,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.regex.Matcher;
