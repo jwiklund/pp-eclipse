ccce6478f14fc32ac7f61c113a4d94dae77f204f
Jonas Wiklund <jwiklund@atex.com>
2010-08-30 09:04:56 +0200

    inlined configuration

--- src/pp/eclipse/command/OpenExternalIdCommand.java
+++ src/pp/eclipse/command/OpenExternalIdCommand.java
@@ -1,12 +1,30 @@
 package pp.eclipse.command;
 
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.swt.widgets.Shell;
+
+import pp.eclipse.cache.Cache;
+import pp.eclipse.common.DialogFactory;
+import pp.eclipse.common.Repository;
 import pp.eclipse.domain.ContentXML;
+import pp.eclipse.domain.ContentXMLFactory;
 import pp.eclipse.domain.ExternalId;
-import pp.eclipse.ui.ContentXMLDialogFactory;
+import pp.eclipse.parse.ContentParser;
+import pp.eclipse.ui.SelectionDialog;
 
 public class OpenExternalIdCommand extends OpenCommandTemplate<ExternalId, ContentXML>
 {
 	public OpenExternalIdCommand() {
-		super(new ContentXMLDialogFactory());
+		super(new DialogFactory<ExternalId, ContentXML>() {
+			@Override
+			public SelectionDialog<ExternalId, ContentXML> createDialog(IContainer root) {
+				Shell shell = new Shell();
+				ContentXMLFactory factory = new ContentXMLFactory(root);
+				Repository<ExternalId, ContentXML> repository = new Repository<ExternalId, ContentXML>(root, factory, new ContentParser());
+				SelectionDialog<ExternalId, ContentXML> dialog = new SelectionDialog<ExternalId, ContentXML>(shell, factory, repository, Cache.<ExternalId, ContentXML>none());
+				dialog.setTitle("Filtered ContentXml Dialog");
+				return dialog;
+			}
+		});
 	}
 }
--- src/pp/eclipse/command/OpenInputTemplateCommand.java
+++ src/pp/eclipse/command/OpenInputTemplateCommand.java
@@ -1,12 +1,30 @@
 package pp.eclipse.command;
 
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.swt.widgets.Shell;
+
+import pp.eclipse.cache.Cache;
+import pp.eclipse.common.DialogFactory;
+import pp.eclipse.common.Repository;
 import pp.eclipse.domain.InputTemplate;
 import pp.eclipse.domain.TemplateDefinition;
-import pp.eclipse.ui.TemplateDefinitionDialogFactory;
+import pp.eclipse.domain.TemplateDefinitionFactory;
+import pp.eclipse.parse.TemplateParser;
+import pp.eclipse.ui.SelectionDialog;
 
 public class OpenInputTemplateCommand extends OpenCommandTemplate<InputTemplate, TemplateDefinition>
 {
 	public OpenInputTemplateCommand() {
-		super(new TemplateDefinitionDialogFactory());
+		super(new DialogFactory<InputTemplate, TemplateDefinition>() {
+			@Override
+			public SelectionDialog<InputTemplate, TemplateDefinition> createDialog(IContainer root) {
+				Shell shell = new Shell();
+				TemplateDefinitionFactory factory = new TemplateDefinitionFactory(root);
+				Repository<InputTemplate, TemplateDefinition> repository = new Repository<InputTemplate, TemplateDefinition>(root, factory, new TemplateParser());
+				SelectionDialog<InputTemplate, TemplateDefinition> dialog = new SelectionDialog<InputTemplate, TemplateDefinition>(shell, factory, repository, Cache.<InputTemplate, TemplateDefinition>none());
+				dialog.setTitle("Filtered TemplateDefinition Dialog");
+				return dialog;
+			}
+		});
 	}	
 }
